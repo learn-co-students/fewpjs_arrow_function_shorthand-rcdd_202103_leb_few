015b7ca2807b3743360f74467cd94f6204a25648
Marwa  Awwad <marwaawwad10@gmail.com>
2021-05-19 00:12:43 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+// function divide(a,b){
+// return   a / b
+// }
+let divide = () => 2000/100
+
+let square = (parameter1) => parameter1*parameter1
+let add = (parameter1, parameter2) => parameter1 + parameter2
